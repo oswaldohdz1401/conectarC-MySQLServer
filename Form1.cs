068d4ee5e758570522ef67871a6cb7174aeae453
Oswaldo Hernández <33918023+oswaldohernandez11@users.noreply.github.com>
2018-04-04 13:28:50 -0500

    Comentarios en codigo

--- Form1.cs
+++ Form1.cs
@@ -20,12 +20,16 @@ namespace WindowsFormsApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
+                                            //Nombre del servidor                   //Nombre Base        //En caso de usuario y contraseña
             string conectar = "Data Source= A-006-4291\\SQLEXPRESS; Initial Catalog = primeraBase; Integrated Security = True; ";
+                            //Ponemos el query que deseamos realizar o mostrar
             string query = "select * from[primeraBase].[dbo].[Table_1]";
+            
             SqlConnection con = new SqlConnection(conectar);
             SqlDataAdapter da = new SqlDataAdapter(query, con);
             DataTable dt = new DataTable();
             da.Fill(dt);
+            //el dataGridView1 es donde se va a mostrar la tabla
             dataGridView1.DataSource = dt;
             
         }
